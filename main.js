98bf6fad2d7c350f930edbc6ac65c033e27d4aec
Hewr Srood <hewr_s@outlook.com>
2020-06-03 02:50:27 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for(const likeBtn of likeBtns){
     console.log(e.target.innerText)
     mimicServerCall()
         .then(() => {
-          changeHeart(e)
+          changeHeart()
         })
         .catch((error) => {
           modal.classList.remove('hidden')
